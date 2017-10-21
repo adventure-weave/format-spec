70178c78509d560c0b972ace15eeb740317eeb0e
Michał Woźniczak <michal.wozniczak@gmail.com>
2017-10-21 17:01:57 +0200

    Added initial version of the spec.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+interface ChoiceSpec {
+    name: string
+    leads_to: string
+    [propName: string]: any
+}
+
+interface NodeSpec {
+    title?: string
+    content: string
+    is_start?: boolean
+    input: "pick" | "type"
+    choices: ChoiceSpec[]
+    [propName: string]: any
+}
+
+interface Story {
+    [node_id: string]: NodeSpec
+}
